7c053f346ecfa269ba12f4cde7e1ae6acb8cd2ae
Alejandro Rodriguez Campelo <alejandrorodriguezcampelo@gmail.com>
2022-09-27 09:33:58 +0200

    Correccion de practicas asta 3T2

--- js/practica2T2.js
+++ js/practica2T2.js
@@ -2,10 +2,9 @@ let nombre = prompt("cual es tu nombre","alex");
 let notaEx1 = prompt("cual es la nota del primer examen?");
 let notaEx2 = prompt("cual es la nota del segundo examen?");
 //necsito castear a int los String
-Number(notaEx1);
-Number(notaEx2);
 
-let notmedia = ( notaEx1 + notaEx2 ) / 2 ;
+
+let notmedia = ( Number(notaEx1) + Number(notaEx2) ) / 2 ;
 
 alert(`hola ${nombre}, la nota media de tus examenes es ${notmedia}`);
 
