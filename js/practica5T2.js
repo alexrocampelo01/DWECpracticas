c2bcb7cfae4e4a5f286d35aea212f7b88e9557ed
Alejandro Rodriguez Campelo <alejandrorodriguezcampelo@gmail.com>
2022-09-28 13:35:42 +0200

    commit fin de la practicas asta la 6 bien echas

--- js/practica5T2.js
+++ js/practica5T2.js
@@ -9,9 +9,24 @@ alert
 */
 
 let valido;
-while (typeof valido != number){
-    let res = prompt("introduce un numero haber si adivinas");
-    valido = Number(res);
-    Number(valido);
+console.log("que pasa si "+ Number(valido));
+
+console.log( " tipo antes del while: "+typeof valido);
+while (valido != "number"){
+    valido = prompt("introduce un numero");
+    console.log("que pasa si "+ Number(valido));
+    console.log("dentro del while tipo de valido = "+typeof valido);
+    let cambio = Number(valido);
+    console.log("prueva del Number"+cambio);
+    if(cambio != 0){
+        if(Number.isInteger(Number(valido))){
+                alert("el numero es "+valido);
+            }else{
+                console.log("no es un numero");
+            }
+    }
+    else{
+        console.log("no es un numero");
+    }
+    
 }
-alert("el numero es "+valido);
--- js/practica6T2.js
+++ js/practica6T2.js
@@ -2,5 +2,6 @@ let n = prompt("dame el numero n");
 let K = prompt("dame un valor k");
 
 for (let i=0; i<=K; i++){
-    
+    let mult = Number(n) * Number(i);
+    console.log(n+" * "+i+" = "+mult);
 }
